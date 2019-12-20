3b044a2ed1fa13140e94018bf0c383f8359c9aec
satyaajeet <49395708+satyaajeet@users.noreply.github.com>
2019-12-20 13:39:07 +0530

    Update use_case_2_hack_ai_thon.py
    added comments

--- use_case_2_hack_ai_thon.py
+++ use_case_2_hack_ai_thon.py
@@ -13,13 +13,14 @@ Work hours are between 0800 hours to 2400 hours
 
 
 
-
+#To get input from user, entry time and exit time
+#'counter' varibale checks the number of attempts of input
 def time_input(counter):
     entry_time=int(input("Enter Starting time(24 hours clock(for 12AM midnight, Enter 2400)):"))
     exit_time=int(input("Enter Exit time(24 hours clock(for 12AM midnight, Enter 2400)):"))
     validity_of_time(entry_time,exit_time)
         
-    
+#To check if the time entered by user is in valid format and between 24 hours cycle, if yes then calculate the minutes of work for that day    
 def validity_of_time(entry_time,exit_time):
     global counter
     if (entry_time>2400 or exit_time>2400 or entry_time>exit_time):
@@ -32,16 +33,16 @@ def validity_of_time(entry_time,exit_time):
         
         
     else:
-        entry_hours=entry_time//100
-        entry_minutes=entry_time%100
+        entry_hours,entry_minutes=entry_time//100,entry_time%100
         start_minute=entry_hours*60+entry_minutes
-
-        exit_hours=exit_time//100
-        exit_minutes=exit_time%100
+        exit_hours,exit_minutes=exit_time//100,exit_time%100
         exit_minute=exit_hours*60+exit_minutes
         
         billed_minutes(start_minute,exit_minute)
-    
+
+#To calculate the bill as per the given condition 
+#1260 is equaivalent minutes for 9PM, when child will be on bed 
+#prime time is when bill rate will be 2.5$ and nonprime time is when bill rate will be 1.75$
 def billed_minutes(start_minute,exit_minute):
     if(start_minute>=1260 and exit_minute>=1260):
         non_prime_time=exit_minute-start_minute
