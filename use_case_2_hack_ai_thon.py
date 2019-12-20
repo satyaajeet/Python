3c780c0950ca665579cd92a3d2634eaf82eac235
satyaajeet <49395708+satyaajeet@users.noreply.github.com>
2019-12-20 11:40:44 +0530

    Add files via upload
    A babysitter charges $2.50 an hour until 9:00 PM when the rate drops to $1.75 an hour (the children are in bed).
    
    Write a program that accepts a starting time and ending time in hours and minutes and calculates the total babysitting bill. You may assume that the starting and ending times are in a single 24-hour period. Partial hours should be appropriately prorated.

---
+++ use_case_2_hack_ai_thon.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 11:16:41 2019
+
+@author: Satyajeet 1029693
+"""
+
+print("""
+          Welcome to Bill Calculator
+          --------------------------
+Work hours are between 0800 hours to 2400 hours
+      """)
+
+
+
+
+def time_input(counter):
+    entry_time=int(input("Enter Starting time(24 hours clock(for 12AM midnight, Enter 2400)):"))
+    exit_time=int(input("Enter Exit time(24 hours clock(for 12AM midnight, Enter 2400)):"))
+    validity_of_time(entry_time,exit_time)
+        
+    
+def validity_of_time(entry_time,exit_time):
+    global counter
+    if (entry_time>2400 or exit_time>2400 or entry_time>exit_time):
+        while(counter<=2):
+            print("\n***ERROR time not in valid format or range Error*** Try Again")
+            print("\nNumber of Attempts",counter)
+            counter+=1
+            time_input(counter)
+            break
+        
+        
+    else:
+        entry_hours=entry_time//100
+        entry_minutes=entry_time%100
+        start_minute=entry_hours*60+entry_minutes
+
+        exit_hours=exit_time//100
+        exit_minutes=exit_time%100
+        exit_minute=exit_hours*60+exit_minutes
+        
+        billed_minutes(start_minute,exit_minute)
+    
+def billed_minutes(start_minute,exit_minute):
+    if(start_minute>=1260 and exit_minute>=1260):
+        non_prime_time=exit_minute-start_minute
+        prime_time=0
+    elif(exit_minute<=1260 and start_minute<=1260):
+        prime_time=exit_minute-start_minute
+        non_prime_time=0
+    else:
+        prime_time=1260-start_minute
+        non_prime_time=exit_minute-1260
+        
+    total_bill=2.5*(prime_time/60)+1.75*(non_prime_time/60)
+    print("work while child was awake {} minutes or {} hours".format(prime_time,(prime_time/60)))
+    print("work after child slept {} minutes or {} hours".format(non_prime_time,(non_prime_time/60)))
+    print("total bill is : {}$".format(total_bill))
+
+counter=1
+time_input(counter) 
+      
+    
+    
